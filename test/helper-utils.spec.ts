6cb65f118de5cc2fe603b28471129df414925736
Saikumar Vantaku <vsaikumar@woundtech.net>
2020-12-22 11:34:45 +0530

    Add a basic test setup

---
+++ test/helper-utils.spec.ts
@@ -0,0 +1,7 @@
+import { addGivenNumbers } from "../src/helper-utils";
+
+describe('Helper utils unit tests', () => {
+    it('should find sum of given numbers', () => {
+        expect(addGivenNumbers(2,5,7)).toEqual(14);
+    });
+});
